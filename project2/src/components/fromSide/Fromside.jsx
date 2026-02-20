74cddbea02e55da5e588b12dc2e17749d47fcab4
26tamzid <earlymorning.bd@gmail.com>
2026-02-20 15:37:15 +0600

    final

--- project2/src/components/fromSide/Fromside.jsx
+++ project2/src/components/fromSide/Fromside.jsx
@@ -1,5 +1,6 @@
 import Button from '../button/Button'
 import styles from './Fromside.module.css'
+import { useState } from 'react';
 import { MdMessage } from "react-icons/md";
 import { FaPhoneAlt } from "react-icons/fa";
 import { MdMailOutline } from "react-icons/md";
@@ -8,6 +9,21 @@ import { MdMailOutline } from "react-icons/md";
 
 
 const Fromside = () => {
+
+
+    const[formData, setFromData]=useState({
+        name:" ",
+        email:" ",
+        text:" "
+    });
+
+    const handel = (e) =>{
+        const {name , value} = e.target;
+        setFromData({
+            ...formData,
+            [name]:value
+        });
+    }
 
     const call_btn =() =>{
         console.log("call clicked")
@@ -25,6 +41,13 @@ const Fromside = () => {
         console.log(   "Name: " + name + "\n" +
         "Email: " + email + "\n" +
         "Text: " + text)
+
+
+        formData({
+            name: "",
+            email: "",
+            text:""
+        })
     }
 
     
@@ -54,21 +77,41 @@ const Fromside = () => {
             className={styles.form2}>
                 <div className={styles.formdiv}>
                     <label htmlFor="name">Name</label>
-                    <input type='text' name="name" placeholder='type...'></input>
+                    <input type='text'
+                     name="name" 
+                     placeholder='type...'
+                     value = {formData.name}
+                     onChange={handel}
+                     ></input>
                 </div>
                 <div className={styles.formdiv}>
                     <label htmlFor="email">Email</label>
-                    <input type='email' name="email" placeholder='type...'></input>
+                    <input type='email'
+                     name="email" 
+                     placeholder='type...'
+                      value = {formData.email}
+                     onChange={handel}
+                     ></input>
                 </div>
                 <div className={styles.formdiv}>
                     <label htmlFor="text">Text</label>
-                    <textarea name="text" placeholder='type...' rows="6" cols="15"></textarea>
+                    <textarea name="text"
+                     placeholder='type...'
+                      value = {formData.text}
+                     onChange={handel}
+                      rows="6" cols="15"></textarea>
                 </div>
                 <Button text="SUBMIT" type='submit'/>
             </form>
+            <div>
+                <p>Name:{formData.name}</p>
+                <p>Email:{formData.email}</p>
+                <p>text:{formData.text}</p>
+               
+            </div>
         </div>
         <div className={styles.imges}>
-            <img src='\public\imges\Service.svg'/>
+            <img src='/imges/Service.svg'/>
         </div>
     </section>
   )
